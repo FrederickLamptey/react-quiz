213e8ad990618c228b5b6be477a46193f19bc3be
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2024-09-06 18:20:34 -0400

    create the various app components.

---
+++ src/components/App.js
@@ -0,0 +1,71 @@
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+import Question from './Question';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+    case 'start':
+      return {
+        ...state,
+        status: 'active',
+      };
+    default:
+      throw new Error('Action unknown');
+  }
+}
+
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+  index: 0,
+};
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { questions, status, index } = state;
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
+  }, []);
+
+  // function handleClick() {
+  //   dispatch({type: 'start'})
+  // }
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === 'active' && <Question question={questions[index]} />}
+      </Main>
+    </div>
+  );
+}
